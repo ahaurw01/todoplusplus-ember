cb1704e9afbdcce92eb970d43c57198b8b88d8bf
ahaurw01 <aaron.haurwitz@gmail.com>
2013-09-14 11:13:14 -0400

    add routes in map.js

--- app/routes/map.js
+++ app/routes/map.js
@@ -3,4 +3,11 @@
 var App = require('application');
 
 App.Router.map(function () {
+  this.resource('products', function () {
+    this.route('category', {path: '/:category'});
+    this.route('product', {path: '/:id'});
+  });
+  this.resource('wishlists', function () {
+    this.route('index', {path: '/:customerId'});
+  });
 });
